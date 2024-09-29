0566a330bb6087aef8b91219344de95a7e764570
Mohit <mohit952096@gmail.com>
2024-09-29 20:43:44 +0530

    Time: 7 ms (85.09%), Space: 10.9 MB (21.74%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,17 +9,32 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_map<ListNode*,bool>hash;
-        ListNode*temp=head;
-        while(temp!=NULL) {
-            if(hash[temp]==true) {
-            return true;
+        // unordered_map<ListNode*,bool>hash;
+        // ListNode*temp=head;
+        // while(temp!=NULL) {
+        //     if(hash[temp]==true) {
+        //     return true;
+        //     }
+        //     else {
+        //         hash[temp]=true;
+        //     }
+        //     temp=temp->next;
+        // }
+        // return false;
+        
+        ListNode*fast= head;
+        ListNode*slow = head;
+        while(fast!=NULL) {
+            fast=fast->next;
+            if(fast!=NULL) {
+                fast=fast->next;
+                slow=slow->next;
             }
-            else {
-                hash[temp]=true;
+            if(fast==slow) {
+                return true;
             }
-            temp=temp->next;
         }
-        return false;
+    return false;
     }
+    
 };
