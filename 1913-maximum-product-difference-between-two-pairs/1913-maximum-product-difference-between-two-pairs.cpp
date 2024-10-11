066c39795773e8fd73ae8955ee6f3c43280fdae2
Mohit <mohit952096@gmail.com>
2024-10-11 20:34:35 +0530

    Time: 26 ms (27.86%), Space: 23.9 MB (53.54%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
-        int maxi=INT_MIN;
-        int smaxi=INT_MIN;
-        for(int n :nums) {
-            if(n>maxi) {
-                smaxi=maxi;
-                maxi=n;
-            }
-            else {
-                smaxi=max(smaxi,n);
-            }
-        }
-        int mini=INT_MAX;
-        int smini=INT_MAX;
-        for(int n:nums) {
-            if(n<mini) {
-                smini=mini;
-                mini=n;
-            }
-            else{
-                smini= min(smini,n);
-            }
-        }
-        return (maxi*smaxi)-(smini*mini);
+        // int maxi=INT_MIN;
+        // int smaxi=INT_MIN;
+        // int mini=INT_MAX;
+        // int smini=INT_MAX;
+        // for(int n :nums) {
+        //     if(n>maxi) {
+        //         smaxi=maxi;
+        //         maxi=n;
+        //     }
+        //     else {
+        //         smaxi=max(smaxi,n);
+        //     }
+        // }
+        
+        // for(int n:nums) {
+        //     if(n<mini) {
+        //         smini=mini;
+        //         mini=n;
+        //     }
+        //     else{
+        //         smini= min(smini,n);
+        //     }
+        // }
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        return (nums[n-1]*nums[n-2])-(nums[0]*nums[1]);
     }
 };
