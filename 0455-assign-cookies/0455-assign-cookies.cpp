56899987687047ce35c40c1ca41692e145ca17c0
Mohit <mohit952096@gmail.com>
2024-10-13 20:50:21 +0530

    Time: 46 ms (32.87%), Space: 44.8 MB (58.10%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int ans=0;
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int m=g.size();
+        int n=s.size();
+        int i=0; int j=0;
+        while(i<m && j<n) {
+            if(s[j] >= g[i]){
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+};
