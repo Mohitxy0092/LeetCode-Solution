c520a5b3651c03b758922c6ad2f972fefeb9fc0e
Mohit <mohit952096@gmail.com>
2024-10-19 10:31:48 +0530

    Time: 0 ms (100.00%), Space: 11.3 MB (67.06%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -3,7 +3,7 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
        int i=m;
         int j=0;
-        while(i<m || j<n) {
+        while(j<n) {
             nums1[i]=nums2[j];
             i++,j++;
         }
