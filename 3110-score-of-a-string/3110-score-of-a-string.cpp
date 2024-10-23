2fdb8c30685d60eaf6b6046b943d55b270deaa5f
Mohit <mohit952096@gmail.com>
2024-10-23 20:49:40 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (26.92%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.cpp
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -3,9 +3,7 @@ public:
     int scoreOfString(string s) {
         int sum=0;
         for(int i=1;i<s.length();i++) {
-            
-            int ans= abs(int(s[i]) - int(s[i-1]));
-            sum+=ans;
+            sum+=( abs(int(s[i]) - int(s[i-1])));
     }
         return sum;
     }
