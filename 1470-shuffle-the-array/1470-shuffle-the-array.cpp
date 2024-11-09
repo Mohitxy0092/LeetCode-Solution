e65b3f172e8c6c000f6b3b075822ff61b6c13496
Mohit <mohit952096@gmail.com>
2024-11-09 19:32:33 +0530

    Time: 8 ms (19.53%), Space: 12.5 MB (29.92%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+     vector<int>v;
+        int i=0;
+        int j =n;
+        while( i<nums.size() && j<nums.size()) {
+            {
+            v.push_back(nums[i]);
+            v.push_back(nums[j]);
+            i++;
+            j++;
+        }
+        }
+        return v;
+    }
+};
