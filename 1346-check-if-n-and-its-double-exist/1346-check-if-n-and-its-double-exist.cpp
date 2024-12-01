f7b1fe2e2fe79269d17355d60344288e54ac5eca
Mohit <mohit952096@gmail.com>
2024-12-01 13:52:54 +0530

    Time: 0 ms (100.00%), Space: 13.6 MB (17.49%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
+    int binary_search(vector<int>&arr,int val) {
+        int s=0;
+        int e=arr.size()-1;
+        while(s<=e) {
+            int mid= s+(e-s)/2;
+            if(arr[mid]==val) {
+                return mid;
+            }
+            else if(arr[mid]<val) {
+                s=mid+1;
+            }
+            else {
+                e=mid-1;
+            }
+        }
+        return -1;
+    }
     bool checkIfExist(vector<int>& arr) {
-        int n = arr.size();
-        unordered_set<int>st;
-        for(int i=0;i<n;i++ ) {
-            if(st.find(arr[i]*2) !=st.end()) {
+        int n=arr.size();
+        sort(begin(arr),end(arr));
+        for(int i =0;i<n;i++) {
+            int index = binary_search(arr,2*arr[i]);
+            if(index!=-1 && index!=i) {
                 return true;
             }
-            else if(arr[i]%2==0 && st.find(arr[i]/2) != st.end()) {
-                return true;
-            }
-            st.insert(arr[i]);
         }
         return false;
     }
