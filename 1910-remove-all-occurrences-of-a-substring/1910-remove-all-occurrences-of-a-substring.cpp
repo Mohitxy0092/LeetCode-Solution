460931083f88ea7f131734c4326bd528de867264
Mohit <mohit952096@gmail.com>
2024-12-08 20:34:15 +0530

    Time: 1 ms (42.62%), Space: 9.5 MB (10.36%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,26 +1,9 @@
 class Solution {
 public:
-    
-    void recurr(string &s , string &part){
-        int found = s.find(part);
-        if(found==string::npos) return;
-        else{
-            string left_part = s.substr(0,found);
-            string right_part = s.substr(part.length()+found ,s.size());
-            s = left_part.append(right_part);
-            recurr(s,part);
-        }
-        // else {
-        //     return;
-        // }
-    }
     string removeOccurrences(string s, string part) {
-    //     while(s.find(part) != string :: npos) {
-    //         s.erase(s.find(part) ,part.length());
-    //     }
-    //     return s;
-        
-        recurr(s,part);
+        while(s.find(part) != string :: npos) {
+            s.erase(s.find(part),part.length());
+        }
         return s;
     }
 };
