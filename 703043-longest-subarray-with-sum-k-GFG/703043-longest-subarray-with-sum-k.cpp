834e8b0012699ad788239deb1cfae3f152dcd7c1
Mohit <MOHIT.230111368@gehu.ac.in>
2026-02-03 14:39:05 +0530

    Added solution - LeetHub

---
+++ 703043-longest-subarray-with-sum-k-GFG/703043-longest-subarray-with-sum-k.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int longestSubarray(vector<int>& arr, int k) {
+        unordered_map<int,int>st;
+        int n=arr.size();
+        int pre=0;
+        int cnt=0;
+        for(int i=0;i<n;++i) {
+            pre+=arr[i];
+            if(pre==k){
+             cnt=i+1;   
+            }
+            if(st.find(pre)==st.end()) {
+                st[pre]=i;
+            }
+            if(st.find(pre-k)!=st.end()) {
+                cnt=max(cnt,i-st[pre-k]);
+            }
+            
+            }
+        return cnt;
+        
+    }
+};
