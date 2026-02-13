dc8554242c06afdab48fca8e294217084d381ea6
Mohit <MOHIT.230111368@gehu.ac.in>
2026-02-13 11:17:37 +0530

    Added solution - LeetHub

--- 700174-left-view-of-binary-tree-GFG/700174-left-view-of-binary-tree.cpp
+++ 700174-left-view-of-binary-tree-GFG/700174-left-view-of-binary-tree.cpp
@@ -15,20 +15,40 @@ public:
 
 class Solution {
   public:
-  int maxi=0;
-    void lft(Node*root,int level,vector<int>&ans) {
-        if(!root) return;
-        if(maxi<level) {
-            ans.push_back(root->data);
-            maxi=level;
+//   int maxi=0;
+//     void lft(Node*root,int level,vector<int>&ans) {
+//         if(!root) return;
+//         if(maxi<level) {
+//             ans.push_back(root->data);
+//             maxi=level;
+//         }
+//         lft(root->left,level+1,ans);
+//         lft(root->right,level+1,ans);
+//     }
+
+    vector<int> lod(Node*root) {
+        if(!root) return {};
+        vector<int>ans;
+        queue<Node*>q;
+        q.push(root);
+        while(!q.empty()) {
+            int currs=q.size();
+            for(int i=0;i<currs;++i) {
+            Node*curr=q.front();
+            q.pop();
+                if(i==0) {
+                    ans.push_back(curr->data);
+                }
+                if(curr->left !=nullptr) q.push(curr->left);
+                if(curr->right !=nullptr) q.push(curr->right);
+            }
         }
-        lft(root->left,level+1,ans);
-        lft(root->right,level+1,ans);
+        return ans;
     }
     vector<int> leftView(Node *root) {
         // code here
-        vector<int>ans;
-        lft(root,1,ans);
-        return ans;
+        // vector<int>ans;
+        // lft(root,1,ans);
+        return lod(root);
     }
 };
