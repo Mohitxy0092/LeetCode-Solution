aaef75f162992cf2fad545885dd47de387400e4b
Mohit <MOHIT.230111368@gehu.ac.in>
2026-03-06 20:52:59 +0530

    Time: 0 ms (100%), Space: 8.2 MB (46.93%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-        return s.find("01")==string::npos;
+        for(int i=1;i<s.length();++i) {
+            if(s[i-1]=='0' && s[i]=='1') return false;
+        }
+        return true;
     }
 };
